a9ed860e5dbeadf15cb568bf6dca60b852c32f0b
maxwooks <mark.pedersen@protonmail.com>
2023-01-12 12:24:39 +0100

    add filepath for key storage in install script

--- kubernetes/install-kubeadm.sh
+++ kubernetes/install-kubeadm.sh
@@ -4,12 +4,13 @@
 
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-
+sudo mkdir /etc/apt/keyrings
 sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 
 sudo apt-get update
-sudo apt-get install -y kubelet=1.26.0-00 kubeadm=1.26.0-00 kubectl=1.26.0-00
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
